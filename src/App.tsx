e02b800a2da17f221583741cda62186c0106a1e6
Кирилл <94886658+Kiri1111@users.noreply.github.com>
2022-10-17 13:52:07 +0300

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
+import React1, {MouseEvent} from "react";
 import './App.css';
+import {Button} from "./Components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+    //     console.log('hello im Vasya')
+    // }
+    // const mySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+    //     console.log('hello im Ivan')
+    // }
+
+    // const onClickHandler = (name: string) => {
+    //     console.log(name)
+    // }
+    // const foo1 = () => {
+    //     console.log(100200)
+    // }
+    //
+    // const foo2 = (cifra: number) => {
+    //     console.log(cifra)
+    // }
+
+    const Button1Foo = (sub: string, age: number) => {
+        console.log(sub, age)
+    }
+
+    const Button2Foo = (sub: string) => {
+        console.log(sub)
+    }
+
+    return (
+        <div className="App">
+            {/*<button onClick={(event) => {*/}
+            {/*    console.log('hello')*/}
+            {/*}}>MyYouTubeChanel-1*/}
+            {/*</button>*/}
+            {/*<button onClick={(event: MouseEvent<HTMLButtonElement>) => onClickHandler('Ivan')*/}
+            {/*}>MyYouTubeChanel-1*/}
+            {/*</button>*/}
+            {/*<button onClick={(event: MouseEvent<HTMLButtonElement>) => onClickHandler('Vasya')*/}
+            {/*}>MyYouTubeChanel-2*/}
+            {/*</button>*/}
+
+            {/*<button onClick={c onClickHandler('some info')*/}
+
+            {/*}>MyYouTubeChanel-3*/}
+            {/*</button>*/}
+
+            {/*<button onClick={foo1}>1</button>*/}
+            {/*<button onClick={() => foo2(100200)}>2</button>*/}
+
+            {/*<button>MyYouTubeChanel-1</button>*/}
+            {/*<button>MyYouTubeChanel-2</button>*/}
+
+            <Button name={'YouTubeChanel-1'} callBack={() => Button1Foo('I am Vasya', 21)}/>
+            <Button name={'YouTubeChanel-2'} callBack={() => Button2Foo('I am Ivan')}/>
+        </div>
+    );
 }
+
 
 export default App;

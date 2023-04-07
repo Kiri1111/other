def1243042836c073725428c033c2eb96787f9b4
Kiri1111 <92_medved@mail.ru>
2023-04-07 12:54:54 +0300

    feat: create store zustand (example)

--- src/Debounce/newComponent/comp.tsx
+++
@@ -1,8 +0,0 @@
-import {ApolloClient, InMemoryCache} from "@apollo/client";
-
-
-export const client = new ApolloClient({
-    uri: 'https://api.github.com/graphql',
-    headers: {"Authorization": 'Bearer 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'},
-    cache: new InMemoryCache()
-})

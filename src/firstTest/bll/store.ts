c3525b5a417d40392b68ca4af22b134443f5ee65
Kiri1111 <92_medved@mail.ru>
2023-04-17 10:09:59 +0300

    feat: test first

--- src/firstTest/bll/store.ts
+++ src/firstTest/bll/store.ts
@@ -2,17 +2,27 @@ import {create} from 'zustand';
 import {immer} from 'zustand/middleware/immer';
 
 import {persist} from 'zustand/middleware';
+import {v1} from "uuid";
+
+type ProductType = {
+    id: string
+    title: string
+    price: number
+}
+
+type AccountState = { productsState: ProductType[] }
 
 
-type AccountState = {}
+const initialValue = [
+    {id: v1(), title: 'Кеды', price: 120},
+    {id: v1(), title: 'Кроксы', price: 100},
+    {id: v1(), title: 'Тапки', price: 90},
+    {id: v1(), title: 'Туфли', price: 200}
+]
 
-const initialValue = {}
 
 export const useCreateShopStore = create(persist(immer<AccountState>((set) => ({
-    userState: {
-        ...initialValue
-    },
-
+    productsState: [...initialValue],
 })), {name: 'shop'}))
 
-// export const selectClearState = (state: AccountState) => state.clearState
+export const selectProduct = (state: AccountState) => state.productsState
--- src/firstTest/ui/mainPage.tsx
+++ src/firstTest/ui/mainPage.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import {selectProduct, useCreateShopStore} from "../bll/store";
 
 export const MainPage = () => {
+
+    const products = useCreateShopStore(selectProduct)
     return (
         <div>
-
             <h2>Товары:</h2>
-
-
+            <ul>
+                {products.map(el =>
+                    <li key={el.id}>{el.title}
+                        <span> Цена: {el.price} руб.</span>
+                    </li>
+                )}
+            </ul>
         </div>
     );
 };

58ca33ccdaa2007d7969b12d23be6676c3bf2c19
Kiri1111 <92_medved@mail.ru>
2023-04-29 21:21:51 +0300

    feat: component selectPhoto

---
+++ src/Debounce/selectPhoto.tsx
@@ -0,0 +1,40 @@
+import {ChangeEvent, FC} from 'react'
+
+import s from './SelectPhoto.module.scss'
+
+import {
+    selectSetFormData,
+    selectSetPhotoUrls,
+    useCreatePostStore,
+} from '@/features/posts/createPostStore'
+
+export const SelectPhoto: FC = () => {
+    const setPhotoUrls = useCreatePostStore(selectSetPhotoUrls)
+    const setFormData = useCreatePostStore(selectSetFormData)
+
+    const onChangeSelect = (e: ChangeEvent<HTMLInputElement>): void => {
+        const files = e.target?.files
+        const urls: string[] = []
+
+        if (files && files.length) {
+            Array.from(files).forEach(file => {
+                urls.push(URL.createObjectURL(file))
+                setFormData('postPhoto', file)
+            })
+            setPhotoUrls(urls)
+        }
+    }
+
+    return (
+        <label className={s.selectPhoto} htmlFor="select-photo">
+            <div className={s.area}>Drag and drop a photo, or select from your computer</div>
+            <input
+                id="select-photo"
+                type="file"
+                accept=".jpg, .jpeg, .png"
+                multiple
+                onChange={onChangeSelect}
+            />
+        </label>
+    )
+}
